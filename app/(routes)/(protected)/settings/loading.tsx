445eb122232f6070fb6271b2ac1d7ea3a508e049
Your Name <cherevko022@gmail.com>
2024-05-24 22:54:32 -0400

    settings loader

--- app/(routes)/(protected)/settings/loading.tsx
+++ app/(routes)/(protected)/settings/loading.tsx
@@ -1,12 +1,33 @@
 import { MaxWidthWrapper } from "@/components/max-width-wrapper";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const SettingsLoader = () => {
   return (
     <div className="w-full h-full py-12">
       <MaxWidthWrapper>
         <Card>
-          <p>loading...</p>
+          <CardHeader>
+            <Skeleton className="h-7 w-[200px]" />
+            <Skeleton className="h-5 w-[300px]" />
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-5">
+              <div className="flex flex-col gap-y-2">
+                <Skeleton className="h-6 w-[100px]" />
+                <Skeleton className="h-10 w-full" />
+              </div>
+              <div className="flex flex-col gap-y-2">
+                <Skeleton className="h-6 w-[100px]" />
+                <Skeleton className="h-10 w-full" />
+              </div>
+              <div className="flex flex-col gap-y-2">
+                <Skeleton className="h-6 w-[100px]" />
+                <Skeleton className="h-10 w-full" />
+              </div>
+              <Skeleton className="h-8 w-[180px]" />
+            </div>
+          </CardContent>
         </Card>
       </MaxWidthWrapper>
     </div>
